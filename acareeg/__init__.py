60f90d3021b68a3d26fcb3eafb0304ecc57e8f3f
Christian O'Reilly <christian.oreilly@gmail.com>
2021-05-12 16:27:59 -0400

    Update __init__.py

--- acareeg/__init__.py
+++ acareeg/__init__.py
@@ -1,7 +1,7 @@
 # Authors: Christian O'Reilly <christian.oreilly@gmail.com>
 # License: MIT
 
-from .eegip import get_connectivity, get_resting_stage_epochs
+from .eegip import get_connectivity, get_resting_state_epochs
 from .infantmodels import compute_sources
 from .visualization import get_plotting_meshes, show_meshes
 
